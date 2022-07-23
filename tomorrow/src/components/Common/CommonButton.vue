fc983b22d4974cb4f14376489d7d4ce2bbdf4798
Mong9 <bean326@naver.com>
2022-07-23 22:31:42 +0900

    ✅ CommonButton Test

---
+++ tomorrow/src/components/Common/CommonButton.vue
@@ -0,0 +1,17 @@
+<template>
+	<button :style="btnWidth">{{ btnText }}</button>
+</template>
+<script>
+export default {
+	name: 'CommonButton',
+	props: {
+		btnWidth: {
+			width: null,
+			required: true,
+		},
+		btnText: {
+			type: String,
+		},
+	},
+};
+</script>
--- tomorrow/src/components/Layouts/TheHeader.vue
+++ tomorrow/src/components/Layouts/TheHeader.vue
@@ -1,17 +1,37 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-4">Header</div>
-			<i class="ic-chevron"></i>
-			<i class="ic-close"></i>
+	<header class="global-header">
+		<div class="gnb">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<i class="ic-chevron"></i>
+						<i class="ic-close"></i>
+						<div class="test">아아아아아아</div>
+						<CommonButton
+							:btnWidth="btnWidth"
+							:btnText="btnText"
+							class="btn-primary btn-40"
+							type="button"
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
+	</header>
 </template>
 
 <script>
+import CommonButton from '../Common/CommonButton.vue';
 export default {
 	name: 'TheHeader',
+	components: { CommonButton },
+	data() {
+		return {
+			btnWidth: {
+				width: '120px',
+			},
+			btnText: '테스트입니다',
+		};
+	},
 };
 </script>
-
-<style lang="scss" scoped></style>
