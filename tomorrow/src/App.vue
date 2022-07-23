20483e9830e57896ecd2737e4df1d096e5d91cc1
Mong9 <bean326@naver.com>
2022-07-24 00:54:36 +0900

    💄 Add Style

--- tomorrow/src/App.vue
+++ tomorrow/src/App.vue
@@ -1,15 +1,17 @@
 <template>
 	<div id="app">
 		<TheHeader />
+		<TheFooter />
 	</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import TheFooter from './components/Layouts/TheFooter.vue';
 import TheHeader from './components/Layouts/TheHeader.vue';
 
 @Component({
-	components: { TheHeader },
+	components: { TheHeader, TheFooter },
 })
 export default class App extends Vue {}
 </script>
