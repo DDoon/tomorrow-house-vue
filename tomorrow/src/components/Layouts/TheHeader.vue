b4b078af70e4c2215c3144f68d8954b324ef6c62
Mong9 <bean326@naver.com>
2022-07-23 16:13:57 +0900

    💄 Add TheHeader Component

---
+++ tomorrow/src/components/Layouts/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">Header</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TheHeader',
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ tomorrow/src/view/ProductDetailPage.vue
@@ -0,0 +1,11 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	name: 'ProductDetailPage',
+};
+</script>
+
+<style lang="scss" scoped></style>
